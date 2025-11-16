b06ee5e8d1168903af14a80775613d798e1eb9ca
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-16 10:34:02 +0600

    feat(reset password ) add auth reset password

--- src/page/auth/ForgotPassword.jsx
+++ src/page/auth/ForgotPassword.jsx
@@ -2,83 +2,124 @@ import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { baseApi } from "../../utils/baseUrl";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";  
+import { MuiOtpInput } from "mui-one-time-password-input";
+import { set } from "react-hook-form";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [otp, setOtp] = useState("");
+  const [step, setStep] = useState("email");  
+  const [ password , setPassword ] = useState("")
 
-  const [isTrue, setIstrue] = useState(false);
-  const pushToDashboard = useNavigate();
+  const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
+
+  const handleSendOtp = async (event) => {
     event.preventDefault();
+
+    if (!email) {
+      toast.error("Email is required");
+      return;
+    }
 
     try {
       const res = await baseApi.post(
         "/auth/user/forgot-password/send-otp/",
-        email
+        { email }
       );
-      const data = await res.data;
-      console.log(data);
 
-      if (res?.status === 200) {
-        setIstrue(true);
-        toast("OTP Send");
+      if (res.status === 200) {
+        console.log(res);
+        toast.success("OTP sent to your email");
+        setStep("otp");
       }
     } catch (error) {
       console.log(error);
+      toast.error("Failed to send OTP");
     }
   };
 
-  const handlVerify = async () => {
-    const inputData = {
-      email: email,
-      otp: otp,
-    };
+
+  const handleVerifyOtp = async () => {
+    if (otp.length !== 6) {
+      toast.error("Enter a valid 6-digit OTP");
+      return;
+    }
 
     try {
       const res = await baseApi.post(
         "/auth/user/forgot-password/verify-otp/",
-        inputData
+        { email, otp }
       );
-      const data = res.data;
-      console.log(data);
 
-      if (res?.status === 200) {
-        pushToDashboard("/");
+      if (res.status === 200) {
+        toast.success("OTP Verified");
+
+        setStep("reset")
+           
       }
     } catch (error) {
       console.log(error);
+      toast.error("Invalid OTP");
     }
   };
 
+
+
+  const handleResetPassword = async ()=>{
+
+    const data ={
+      email:email,
+      otp:otp,
+      new_password:password
+    }
+
+    try {
+
+      const res = await baseApi.post('/auth/user/forgot-password/reset/',data);
+
+      console.log(res)
+
+      if(res?.status===200)
+      {
+        navigate('/');
+      }
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-indigo-700 p-4">
-      {isTrue === false && (
+
+     
+      {step === "email" && (
         <form className="md:w-1/2 bg-white p-8 rounded ">
-          <TextField placeholder="Enter your email" className="w-full" />
+          <TextField
+            placeholder="Enter your email"
+            className="w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <Button
-            onClick={handleSubmit}
-            type="submit"
-            variant="contained"
+            onClick={handleSendOtp}
             fullWidth
+            variant="contained"
             className="!mt-3 !py-3 !rounded-xl !text-lg !bg-blue-600 hover:!bg-blue-700"
           >
-            Send Otp
+            Send OTP
           </Button>
         </form>
       )}
 
-      {isTrue === true && (
+      
+      {step === "otp" && (
         <form className="md:w-1/2 bg-white p-8 rounded ">
-          <TextField
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-            className="w-full"
-          />
-
           <MuiOtpInput
             length={6}
             value={otp}
@@ -86,17 +127,54 @@ function ForgotPassword() {
             className="mb-6 mt-3"
             TextFieldsProps={{ placeholder: "-" }}
           />
+
           <Button
-            onClick={handlVerify}
-            type="submit"
-            variant="contained"
+            onClick={handleVerifyOtp}
             fullWidth
+            variant="contained"
             className="!mt-3 !py-3 !rounded-xl !text-lg !bg-blue-600 hover:!bg-blue-700"
           >
-            Send Otp
+            Verify OTP
           </Button>
         </form>
       )}
+
+
+      {/* last step */}
+
+
+      {
+        step==='reset' && (
+          <form>
+              <TextField
+            placeholder="Enter your email"
+            className="w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <MuiOtpInput
+            length={6}
+            value={otp}
+            onChange={setOtp}
+            className="mb-6 mt-3"
+            TextFieldsProps={{ placeholder: "-" }}
+          />
+          <TextField onChange={(e)=> setPassword(e.target.value)} placeholder="Enter your new password" className="w-full" value={password}/>
+
+             <Button
+            onClick={handleResetPassword}
+            fullWidth
+            variant="contained"
+            className="!mt-3 !py-3 !rounded-xl !text-lg !bg-blue-600 hover:!bg-blue-700"
+          >
+            Change Password
+          </Button>
+
+          </form>
+        )
+      }
+
+
     </div>
   );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,13 +6,14 @@ import UserOverViewChart from "../page/dashboard/chart/UserOverViewChart";
 import Setting from "../page/dashboard/setting/Setting";
 import AuthPage from "../page/auth/AuthPage";
 
-import RestPassword from "../page/auth/RestPassword";
+import RestPassword from "../page/auth/ResetPassword";
 import ForgotPassword from "../page/auth/ForgotPassword";
 import EmailVerification from "../page/auth/EmailVerification";
 import Login from "../page/auth/Login";
 import Register from "../page/auth/Register";
 import OtpVerification from "../page/auth/OtpVerification";
 import Profile from "../page/Profile";
+import ResetPassword from "../page/auth/ResetPassword";
 
 export const route = createBrowserRouter([
   {
@@ -35,58 +36,57 @@ export const route = createBrowserRouter([
         path: "setting",
         element: <Setting />,
       },
-        // profile
-  {
-    path:'/profile',
-    element:<Profile/>
-  }
+      // profile
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
     ],
   },
   // auth
 
   {
     path: "/auth",
-    element:<AuthPage/>,
-    children:[
-        {
-            path:'login',
-            element:<Login/>
-        },
-        {
-            path:'sign-up',
-            element:<Register/>
-        },
-        {
-            path:'reset-password',
-            element:<RestPassword/>
-        },
-        {
-            path:'forgot-password',
-            element:<ForgotPassword/>
-        },
-        {
-            path:'email-verification',
-            element:<EmailVerification/>
-        },
-        {
-          path:'otp-verify',
-          element:<OtpVerification/>
-        },
-        {
-          path:"forgot-password",
-          element:<ForgotPassword/>
-        }
-    ]
+    element: <AuthPage />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "sign-up",
+        element: <Register />,
+      },
+      {
+        path: "reset-password",
+        element: <RestPassword />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "email-verification",
+        element: <EmailVerification />,
+      },
+      {
+        path: "otp-verify",
+        element: <OtpVerification />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+    ],
   },
-  // forgot password
+  //  password
 
   {
-          path:"/forgot-password",
-          element:<ForgotPassword/>
-        }
-
-
-
-
-
+    path: "forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path:'reset-password',
+    element:<ResetPassword/>
+  }
 ]);

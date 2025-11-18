2d62bce8ce43986c43119491a9fed2fb1966ecb0
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-18 11:15:23 +0600

    feat(update profile) add rtk query on update profile

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -1,14 +1,13 @@
 import { TextField } from "@mui/material";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
 import toast, { Toaster } from "react-hot-toast";
-
+import { useUpdateProfileMutation } from "../redux/features/api";
 function Profile() {
   const [loading, setLoading] = useState(false);
   const token = localStorage.getItem("token");
-    
-const [updateProfile] = useUpdateProfileMutation();
+
+  const [updateProfile] = useUpdateProfileMutation();
 
   const {
     register,
@@ -16,43 +15,47 @@ const [updateProfile] = useUpdateProfileMutation();
     formState: { errors },
   } = useForm();
 
+  const onSubmit = async (data) => {
+    setLoading(true);
 
+    try {
+      const res = await updateProfile({ data, token }).unwrap();
 
-const onSubmit = async (data) => {
-  setLoading(true);
-  
-
-  try {
-    const res = await updateProfile({ data, token }).unwrap(); 
-
-    toast.success("Profile updated successfully");
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-    toast.error(error?.data?.detail || "Failed to update profile");
-  } finally {
-    setLoading(false);
-  }
-};
+      toast.success("Profile updated successfully");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.data?.detail || "Failed to update profile");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center flex-col w-full p-4">
       <Toaster />
 
-      <h3 className="md:text-3xl text-2xl font-semibold">Update your profile</h3>
+      <h3 className="md:text-3xl text-2xl font-semibold">
+        Update your profile
+      </h3>
 
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col w-full gap-y-4 mt-10 justify-center mx-auto items-center"
       >
-        <img className="h-20 w-20 rounded-full " src="https://img.icons8.com/material-rounded/24/person-male.png"/>
+        <img
+          className="h-20 w-20 rounded-full "
+          src="https://img.icons8.com/material-rounded/24/person-male.png"
+        />
         <TextField
           {...register("username", { required: true })}
           variant="outlined"
           className="md:w-1/2 w-full"
           placeholder="Enter your username"
         />
-        {errors.username && <span className="text-red-500">This field is required</span>}
+        {errors.username && (
+          <span className="text-red-500">This field is required</span>
+        )}
 
         <TextField
           {...register("first_name", { required: true })}
@@ -60,7 +63,9 @@ const onSubmit = async (data) => {
           className="md:w-1/2 w-full"
           placeholder="Enter your first name"
         />
-        {errors.first_name && <span className="text-red-500">This field is required</span>}
+        {errors.first_name && (
+          <span className="text-red-500">This field is required</span>
+        )}
 
         <TextField
           {...register("last_name", { required: true })}
@@ -68,12 +73,13 @@ const onSubmit = async (data) => {
           className="md:w-1/2 w-full"
           placeholder="Enter your last name"
         />
-        {errors.last_name && <span className="text-red-500">This field is required</span>}
+        {errors.last_name && (
+          <span className="text-red-500">This field is required</span>
+        )}
 
         <TextField
           {...register("email", {
             required: true,
-          
           })}
           variant="outlined"
           className="md:w-1/2 w-full"
@@ -81,11 +87,9 @@ const onSubmit = async (data) => {
         />
         {errors.email && (
           <span className="text-red-500">
-             {errors.email.message || "This field is required"}
+            {errors.email.message || "This field is required"}
           </span>
         )}
-
-        
 
         <div className="mt-4">
           <button

fb913fad774a221c17bb5101ed08d81124ebcb7f
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-25 16:52:20 +0600

    feat(dashboard) add total items on dashboard page

--- src/components/ui/SearchButton.jsx
+++ src/components/ui/SearchButton.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 function SearchButton({title, onClick}) {
   return (
-    <button onClick={onClick} className='h-12  md:mt-0 bg-[#3B82F6]  rounded text-white md:w-44 md:w-36 md:text-xl md:font-semibold w-1/2  '>{title}</button>
+    <button onClick={onClick} className='h-12  md:mt-0 bg-blue-600  rounded text-white md:w-44 md:w-36 md:text-xl md:font-semibold w-1/2  '>{title}</button>
   )
 }
 
--- src/page/dashboard/inventory/DashboardStats.jsx
+++ src/page/dashboard/inventory/DashboardStats.jsx
@@ -1,11 +1,26 @@
 import { Paper } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardComponent from "../../../components/ui/CardComponent";
 import { FiBook, FiBox, FiLayers } from "react-icons/fi";
 import InventoryTable from "../../../components/ui/InventoryTable";
+import { useAllInventoryQuery } from "../../../redux/features/api";
 
 function DashboardStats() {
-  
+
+  const {data:allInventory} = useAllInventoryQuery();
+
+  const [ totalItems , setTotalItems ] = useState("");
+
+  useEffect(()=>{
+
+    const total = allInventory?.reduce((acc, value)=> acc+ Number (value.number) , 0);
+    setTotalItems(total);
+
+  },[])
+
+
+
+
   const [inventoryList, setInventoryList] = useState([
     { name: "Headphone", category: "IT", priority: "High", number: 10 },
     { name: "Keyboard", category: "IT", priority: "Medium", number: 5 },
@@ -20,7 +35,7 @@ function DashboardStats() {
     <div className="w-full text-black p-4 md:p-6 flex flex-col justify-center -mr-10 h-screen">
       {/* Cards Section */}
       <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mb-6">
-        <CardComponent Icon={<FiBox size={28} />} number={100} title="Total Items" />
+        <CardComponent Icon={<FiBox size={28} />} number={totalItems} title="Total Items" />
         <CardComponent Icon={<FiLayers size={28} />} number={8} title="Total Category" />
         <CardComponent Icon={<FiBook size={28} />} number={150} title="Total Stock" />
       </section>
